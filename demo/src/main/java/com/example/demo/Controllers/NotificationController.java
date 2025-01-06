10a26ff567949aad37cf1c600042d34d953a946e
GiharaNavindu <raviherath1961@gamil.com>
2025-01-06 16:46:08 +0530

    clients and server

--- demo/src/main/java/com/example/demo/Controllers/NotificationController.java
+++ demo/src/main/java/com/example/demo/Controllers/NotificationController.java
@@ -2,15 +2,17 @@ package com.example.demo.Controllers;
 
 
 import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class NotificationController {
 
 
-    //the web scket endpoint is 
+    //the web scket endpoint is /app/sendMessage
     @MessageMapping("/sendMessage") //like Get/P ostmapping in the webendpoints for web sockets to access by the stomp clients
+    @SendTo("/topic/notification")/// the msg will only visble to this brooker
     public String sendMessage(String message) {
-        return message;//just  returning the message
+        return message;//just  returning the message  and message will be sent to tje broker
     }
 }
